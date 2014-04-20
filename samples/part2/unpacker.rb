0d664feac69bcae10bf7c94883ec7ab6657e30f1
Benoit Daloze <eregontp@gmail.com>
2014-04-20 19:01:23 +0200

    implement unpack

--- samples/part2/unpacker.rb
+++ samples/part2/unpacker.rb
@@ -3,7 +3,30 @@ module Unpacker
     # This method takes an array of bytes in message pack format and convert 
     # it into an equivalent Ruby object
     def unpack(bytes)
-      {} # FIXME: Your code goes here.
+      type = bytes.next
+      case type
+      when 0x00..0x7f then type
+      when 0xa0..0xbf
+        unpack_str(type - 0xa0, bytes).force_encoding('UTF-8')
+      when 0xc0 then nil
+      when 0xc2 then false
+      when 0xc3 then true
+      when 0xcb
+        unpack_str(8, bytes).unpack('G')[0]
+      when 0x80..0x8f
+        (type - 0x80).times.with_object({}) { |_,map|
+          map[unpack(bytes)] = unpack(bytes)
+        }
+      else
+        raise "Unknown msgpack type: #{type.to_s(16)}"
+      end
+    end
+
+  private
+    def unpack_str(bytesize, bytes)
+      bytesize.times.with_object("".b) { |_,str|
+        str << bytes.next
+      }
     end
   end
 end
