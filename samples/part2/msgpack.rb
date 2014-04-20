5f473ea866e41149ef54e20b4db95523399731df
Benoit Daloze <eregontp@gmail.com>
2014-04-20 20:48:20 +0200

    stricter validation for Bignums

--- samples/part2/msgpack.rb
+++ samples/part2/msgpack.rb
@@ -1,7 +1,7 @@
 module MsgPack
   EXTENDED_TYPES = { # class => unpack from string (by #to_s)
     Symbol => -> data { data.to_sym },
-    Bignum => -> data { data.to_i }
+    Bignum => -> data { Integer(data) },
   }
   TYPE2EXT = EXTENDED_TYPES.keys
   EXT2TYPE = Hash[TYPE2EXT.each_with_index.to_a]
