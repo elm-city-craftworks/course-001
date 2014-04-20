a7304c48d59864b6d0ca06c0024bd464411aa618
Benoit Daloze <eregontp@gmail.com>
2014-04-20 19:43:18 +0200

    add a test for Array

---
+++ samples/part2/tests.rb
@@ -0,0 +1,17 @@
+require_relative "packer"
+require_relative "unpacker"
+
+def test(input)
+  output = Unpacker.unpack(Packer.pack(input).each)
+
+  print input.inspect
+  if input == output
+    puts " Round-trip OK"
+  else
+    abort "Failed Round-trip\n\n" +
+          "After packing and unpacking, got this:\n\n" +
+          output.inspect
+  end
+end
+
+test([1, "a", 3.14])
