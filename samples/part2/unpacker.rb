429d33985d29bddb7e45d5cc2ae87793f886165c
Benoit Daloze <eregontp@gmail.com>
2014-04-21 14:06:17 +0200

    clean up files by removing duplicated tests

--- samples/part2/unpacker.rb
+++ samples/part2/unpacker.rb
@@ -4,8 +4,6 @@ module Unpacker
   extend self
   include MsgPack
 
-  # This method takes an array of bytes in message pack format and convert
-  # it into an equivalent Ruby object
   def unpack(bytes)
     type = bytes.next
     case type
@@ -60,23 +58,5 @@ private
     bytesize.times.with_object("".b) { |_,str|
       str << bytes.next
     }
-  end
-end
-
-# --------------------------------------------------------------------
-# Run the following tests by executing this file.
-
-if __FILE__ == $PROGRAM_NAME
-  data     = File.binread(File.dirname(__FILE__) + "/example.msg").each_byte
-  expected = {"a"=>1, "b"=>true, "c"=>false, "d"=>nil, "egg"=>1.35}
-
-  actual = Unpacker.unpack(data)
-
-  if actual == expected
-    puts "You unpacked the message correctly!"
-  else
-    abort "Unpacked message is not what was expected.\n\n" +
-          "expected output:\n#{expected.inspect}\n\n" +
-          "actual output:\n#{actual.inspect}"
   end
 end
