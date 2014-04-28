7d15d94e902c938f51c3d3666ec8953482f5530a
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2014-04-28 23:47:23 +0200

    Basic structure, support fixmap (untested)

--- samples/part2/unpacker.rb
+++ samples/part2/unpacker.rb
@@ -1,11 +1,35 @@
 module Unpacker
-  class << self
-    # This method takes a sequence of bytes in message pack format and convert 
-    # it into an equivalent Ruby object
-    def unpack(bytes)
-      {} # FIXME: Your code goes here.
+  class Reader
+    def initialize(bytes)
+      @bytes = bytes
+    end
+
+    def unpack
+      type_byte = @bytes.peek
+      case
+      when type_byte & 0x80 > 0 then unpack_fixmap
+      else raise "Unknown type %.2x" % type_byte
+      end
+    end
+
+    private
+
+    def unpack_fixmap
+      puts "fixmap"
+      num_items = @bytes.next & 0x0F
+      num_items.times.map { unpack }
     end
   end
+
+
+  class << self
+    # This method takes a sequence of bytes in message pack format and convert
+    # it into an equivalent Ruby object
+    def unpack(bytes)
+      Reader.new(bytes).unpack
+    end
+  end
+
 end
 
 # --------------------------------------------------------------------
@@ -15,7 +39,7 @@ if __FILE__ == $PROGRAM_NAME
   data     = File.binread(File.dirname(__FILE__) + "/example.msg").each_byte
   expected = {"a"=>1, "b"=>true, "c"=>false, "d"=>nil, "egg"=>1.35}
 
-  actual = Unpacker.unpack(data) 
+  actual = Unpacker.unpack(data)
 
   if actual == expected
     puts "You unpacked the message correctly!"
