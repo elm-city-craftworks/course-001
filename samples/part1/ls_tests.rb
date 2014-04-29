09970fa18876adbd682b4b53373754c4145d239b
Gregory Brown <gregory.t.brown@gmail.com>
2014-04-29 09:21:23 -0400

    Update ls_tests.rb

--- samples/part1/ls_tests.rb
+++ samples/part1/ls_tests.rb
@@ -8,14 +8,20 @@ check("No arguments", "")
 # TODO: Uncomment each test below and get it to pass.
 
 # check("Dir listing", "foo")
+
 # check("File glob", "foo/*.txt")
+
 # check("Detailed output", "-l")
+
 # check("Hidden files", "-a")
+
 # check("Hidden files with detailed output", "-a -l")
+
 # check("File glob with detailed output", "-l foo/*.txt")
+
 # check("Invalid directory", "missingdir")
 
-check("Invalid flag", "-Z")
+# check("Invalid flag", "-Z")
 
 puts "You passed the tests, yay!"
 
