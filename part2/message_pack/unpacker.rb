da5f0e27ccbe4f86223238b1c618a83a5277f877
Gregory Brown <gregory.t.brown@gmail.com>
2014-05-01 12:08:50 -0400

    Cleanup and make a bit more DRY

--- part2/message_pack/unpacker.rb
+++ part2/message_pack/unpacker.rb
@@ -3,42 +3,43 @@
 module Unpacker
   SYMBOL_TYPE = 1
 
-  # FIXME: What validations would be needed to make sure this code either
-  # always works correctly, or fails with an appropriate error?
+  class << self
+    # This method takes a sequence of bytes in message pack format and convert 
+    # it into an equivalent Ruby object
+    def unpack(bytes)
+      case code = bytes.next
+      when 0x00 .. 0x7f
+        code
+      when 0x80 .. 0x8f
+        size = code & 0x0f
 
-  # This method takes a sequence of bytes in message pack format and convert 
-  # it into an equivalent Ruby object
-  def self.unpack(bytes)
-    case code = bytes.next
-    when 0x00 .. 0x7f
-      code
-    when 0x80 .. 0x8f
-      len = code & 0x0f
+        size.times.with_object({}) { |_,e| e[unpack(bytes)] = unpack(bytes) }
+      when 0xa0..0xbf
+        size = code & 0b000_11111
 
-      len.times.with_object({}) { |_,e| e[unpack(bytes)] = unpack(bytes) }
-    when 0xa0..0xbf
-      len = code & 0b000_11111
+        binary_string(size, bytes).force_encoding("UTF-8")
+      when 0xc0
+        nil
+      when 0xc2
+        false
+      when 0xc3
+        true
+      when 0xcb
+        binary_string(8, bytes).unpack("G").first
+      when 0xc7
+        size = bytes.next
+        type = bytes.next
 
-      len.times.map { bytes.next }
-               .pack("C*").force_encoding("UTF-8")
-    when 0xc0
-      nil
-    when 0xc2
-      false
-    when 0xc3
-      true
-    when 0xcb
-      8.times.map { bytes.next }.pack("C*").unpack("G").first
-    when 0xc7
-      size = bytes.next
-      type = bytes.next
-      
-      case type
-      when SYMBOL_TYPE
-        size.times.map { bytes.next }.pack("U*").to_sym
-      else
-        raise NotImplementedError
+        raise NotImplementedError unless SYMBOL_TYPE == type
+        
+        binary_string(size, bytes).to_sym
       end
+    end
+
+    private
+
+    def binary_string(num, bytes)
+      num.times.map { bytes.next }.pack("C*")
     end
   end
 end
