dda38f34afa0095390a5eb7531a51c87a350ec87
ivanbrennan <ivan.brennan@gmail.com>
2016-05-06 23:19:44 -0400

    use bit masks for permissions

--- samples/part1/lib/rls/permissions.rb
+++ samples/part1/lib/rls/permissions.rb
@@ -1,49 +1,36 @@
 module RLs
   class Permissions
-    FIELDS = [:owner, :group, :world]
+    ACTORS = [:owner, :group, :other]
     OPERATIONS = [:r, :w, :x]
-    MINUS = '-'.freeze
 
     def initialize(file_stat)
       @fmode = file_stat.mode
     end
 
     def to_s
-      rwx = field_components.map do |bit_field|
-        permitted_operations(bit_field) do |bit, operation|
-          enabled?(bit) ? operation : MINUS
-        end
-      end
-
-      rwx.join
+      symbols.join
     end
 
     private
 
-    def field_components
-      mode_components(@fmode, FIELDS, bits_per_component: OPERATIONS.count)
-    end
-
-    def permitted_operations(bit_field, &block)
-      mode_components(bit_field, OPERATIONS, bits_per_component: 1, &block)
-    end
-
-    def mode_components(mode, components, bits_per_component:)
-      max_index = components.count - 1
-      cardinality = 2 ** bits_per_component
-
-      components.each_with_index.map do |component, index|
-        lesser_components = max_index - index
-        bit_offset = lesser_components * bits_per_component
-
-        val = (mode >> bit_offset) % cardinality
-        val = yield(val, component) if block_given?
-        val
+    def symbols
+      bit_masks.map do |mask, operation|
+        enabled?(@fmode & mask) ? operation : :-
       end
     end
 
-    def enabled?(bit)
-      bit > 0
+    def bit_masks
+      msb_masks = lsb_masks.reverse
+      msb_masks.zip(OPERATIONS.cycle)
+    end
+
+    def lsb_masks
+      bits = (ACTORS.count * OPERATIONS.count)
+      bits.times.map { |offset| 0b1 << offset }
+    end
+
+    def enabled?(bits)
+      bits > 0
     end
   end
 end
