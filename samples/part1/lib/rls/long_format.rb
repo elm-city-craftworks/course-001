5face41aaaa2df34f86799c137fdb3ff19a8ab7b
ivanbrennan <ivan.brennan@gmail.com>
2016-05-10 08:06:36 -0400

    directly use instance vars internally
    Defining private attr_readers does nothing but obscure what's going on. Using instance
    variables directly within the class gives a nice visual reminder of where these values
    came from.

--- samples/part1/lib/rls/long_format.rb
+++ samples/part1/lib/rls/long_format.rb
@@ -18,44 +18,42 @@ module RLs
     end
 
     def to_s
-      "#{mode} #{links} #{owner}  #{group}#{bytes} #{last_modified} #{path}"
+      "#{mode} #{links} #{owner}  #{group} #{bytes} #{last_modified} #{path}"
     end
 
     private
 
-    attr_reader :file, :stat, :permissions
-
     def mode
-      ftype = FTYPES[stat.ftype]
-      "#{ftype}#{permissions}"
+      ftype = FTYPES[@stat.ftype]
+      "#{ftype}#{@permissions}"
     end
 
     def links
-      nlink = stat.nlink
+      nlink = @stat.nlink
       nlink.to_s.rjust(2)
     end
 
     def owner
-      `id -un #{stat.uid}`.chomp
+      `id -un #{@stat.uid}`.chomp
     end
 
     def group
-      `id -gn #{stat.uid}`.chomp
+      `id -gn #{@stat.uid}`.chomp
     end
 
     def bytes
-      size = stat.size
-      col_width = @max_byte_count.to_s.length + 2
+      size = @stat.size
+      col_width = @max_byte_count.to_s.length + 1
       size.to_s.rjust(col_width)
     end
 
     def last_modified
-      time = stat.mtime
+      time = @stat.mtime
       time.strftime('%b %e %H:%M')
     end
 
     def path
-      file.path
+      @file.path
     end
   end
 end
